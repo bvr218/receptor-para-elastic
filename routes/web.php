c2349e8624da3e6734cd7f97c24cb1837128dbe0
bvr218 <106783311+bvr218@users.noreply.github.com>
2025-02-15 21:28:09 -0500

    ahora la base apunta tambien

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiRequestController;
+
 
 Route::get('/', function () {
     return redirect("/admin");
 });
+
+
+Route::post('/', [ApiRequestController::class, 'Request'])->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
+Route::put('/', [ApiRequestController::class, 'Request'])->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
+Route::get('/', [ApiRequestController::class, 'Request']);
+Route::delete('/', [ApiRequestController::class, 'Request'])->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
