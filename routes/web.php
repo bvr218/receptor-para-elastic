3016889742fc9717bfa4c094338f984d9f37cbc9
bvr218 <106783311+bvr218@users.noreply.github.com>
2025-03-11 08:32:45 -0500

    .

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiRequestController;
 
 
-Route::get('/', function () {
-    return redirect("/admin");
-});
 
 
-Route::post('/', [ApiRequestController::class, 'captureRequest'])->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
-Route::put('/', [ApiRequestController::class, 'captureRequest'])->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
-Route::get('/', [ApiRequestController::class, 'captureRequest']);
-Route::delete('/', [ApiRequestController::class, 'captureRequest'])->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
+Route::any('/', [ApiRequestController::class, 'captureRequest'])->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
